b0b4e58395cd90153d58844143a1a7e22944c627
yenkuanlee <yenkuanlee@gmail.com>
2017-04-18 17:57:58 +0800

    testtest

--- test/Dmqtt.py
+++ test/Dmqtt.py
@@ -1,7 +1,9 @@
 import paho.mqtt.client as mqtt
 import time
+import math
 
 EventDict = dict()
+DistanceDict = dict()
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, rc):
@@ -25,6 +27,25 @@ def distance(lon1, lat1, lon2, lat2):
 	c = 2 * asin(sqrt(a)) 
 	m = 6367 * c * 1000
 	return m
+
+'''
+def CheckPointInMcc(p,Mcc):
+	global DistanceDict
+	if len(Mcc) == 2:
+		if DistanceDict[(Mcc[0],p)]**2 + DistanceDict[(Mcc[1],p)]**2 > DistanceDict[(Mcc[0],Mcc[1])]**2:
+			return False
+		else return True
+	elif len(Mcc) == 3:
+	else return "ERROR"
+
+def GetNewMcc(p,OldMcc):
+	global DistanceDict
+	if len(Mcc) == 2:
+		if DistanceDict[(Mcc[0],p)]**2 + DistanceDict[(Mcc[1],p)]**2 <= DistanceDict[(Mcc[0],Mcc[1])]**2:
+			return OldMcc
+		else:
+'''
+			
 		
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
@@ -38,6 +59,7 @@ def on_message(client, userdata, msg):
 
 		# Initial
 		global EventDict
+		global DistanceDict
 		RedIndex = set()
 		BlackIndex = set()
 		Neighber = set()
@@ -46,9 +68,11 @@ def on_message(client, userdata, msg):
 		Lat = tmp[1]
 		Time = tmp[2]
 
+		Eid = long(str(time.time()).replace(".",""))
+
 		# Generate Red, Black and Neighber
 		for x in EventDict:
-			Tdiff = Time - EventDict[x]["Time"]
+			Tdiff = Time - EventDict[x]["Time"]	############################
 			if Tdiff <= 1 :
 				RedIndex.add(x)
 			elif Tdiff <= 2 and Tdiff > 1 :
@@ -56,28 +80,69 @@ def on_message(client, userdata, msg):
 			else :
 				EventDict.pop(x,None)
 				continue
-			if distance(Lon,Lat,EventDict[x]["Lon"],EventDict[x]["Lat"]) <= (2*D) :
+			DistanceTmp = distance(Lon,Lat,EventDict[x]["Lon"],EventDict[x]["Lat"]) <= (2*D)
+			if DistanceTmp <= (2*D) :
 				Neighber.add(x)
+				DistanceDict[(x,Eid)] = DistanceTmp	# from small to large
 		
 		results = dict()
 
 
+		# Get L2 and L3
+		L2 = list()
+		L3 = list()
+		for x in Neighber:
+			L2.append(set([x,Eid]),(x,Eid))
+			for y in EventDict[x]["Neighber"]: # Eid > x > y
+				if y not in Neighber:
+					continue
+				a = DistanceDict[(x,Eid)]
+				b = DistanceDict[(y,Eid)]
+				c = DistanceDict[(y,x)]
+				Emax = a
+				Emin = (b,c)
+				diameter = (x,Eid)
+				if b > Emax:
+					Emax = b
+					Emin = (a,c)
+					diameter = (y,Eid)
+				if c > Emax:
+					Emax = c
+					Emin = (a,c)
+					diameter = (y,x)
+				if Emin[0]**2 + Emin[1]**2 <= Emax**2:
+					L3.append(set(y,x,Eid),diameter,Emax/2)
+				else:
+					S = (a+b+c)/2
+					R = a*b*c/(4*math.sqrt(S*(S-a)*(S-b)*(S-c)))
+					if R > 2*D:
+						continue
+					L3.append(set(y,x,Eid),(y,x,Eid),R)
+
+		#results[0] = L2
+		#results[1] = L3
+		
+		# L3 to Ln
+
+
+		'''
 		# Get L2
 		L2 = list()
 		for x in Neighber:
 			for y in EventDict[x]["Neighber"]:
 				if y in Neighber :
-					L2.append(list([y,x]))	# from small to large
+					# (EventSet,MCC)
+					L2.append(list([y,x]),(y,x))	# from small to large
 		results[0] = L2
 
 
 		# Get Ln
 		for x in Neighber:
 			 for i in range(len(EventDict[x]["results"])):
-				for y in EventDict[x]["results"][i]:
+				for y in EventDict[x]["results"][i]:	# (EventSet,MCC)
 					Ltmp = list()
 					Lflag = False
-					for z in y:
+					for z in y[0]:
 						if z in Neighber:
 							Ltmp.append(z)
 						else:
@@ -87,13 +152,17 @@ def on_message(client, userdata, msg):
 					Ltmp.append(x)
 					if i+1 not in results:
 						results[i+1] = list()
-					else:
-						results[i+1].append(Ltmp)
 
+					# Get MCC of Ltmp !!!
+					OldMcc = y[1]
+					NewMcc = GetMcc(x,OldMcc)	############################
+					results[i+1].append(Ltmp,NewMcc)
+
+		'''
 
 
 		# Add To EventDict
-		Eid = long(str(time.time()).replace(".",""))
+
 		EventDict[Eid] = dict()
 		EventDict[Eid]["Lon"] = Lon
 		EventDict[Eid]["Lat"] = Lat
@@ -102,10 +171,6 @@ def on_message(client, userdata, msg):
 		EventDict[Eid]["results"] = results
 
 		print EventDict
-
-		
-
-
 
 client = mqtt.Client()
 client.on_connect = on_connect
